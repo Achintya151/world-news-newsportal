c546d05b3ca3fcf790c3343ec942530ec9402498
Achintya151 <achintyakumartalukdar@gmail.com>
2022-09-04 01:33:22 +0600

    modal added

--- js/index.js
+++ js/index.js
@@ -18,16 +18,64 @@ const setAllMenu = async () => {
         if (uniqueArray.indexOf(data.category_name) === -1) {
             uniqueArray.push(data.category_name);
             const li = document.createElement('li');
-            li.innerHTML = `<a onClick="loadNews('${data.category_id}');">${data.category_name}</a>`
+            li.innerHTML = `<a id="menu-key" onClick="loadNews('${data.category_id}');">${data.category_name}</a>`
             menu.appendChild(li);
         }
 
     }
 }
+
 setAllMenu();
+
 const loadNews = async (categoryId) => {
     const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.data);
+    displayNews(data.data);
+    return data.data;
+}
+
+const displayNews = allnews => {
+    // console.log(allnews);
+    const newsContainer = document.getElementById('news-container');
+    newsContainer.textContent = '';
+    allnews.forEach(news => {
+        const { title, details, thumbnail_url, rating, author } = news;
+        const newsDiv = document.createElement('div');
+        newsDiv.innerHTML = `<div class="card card-side bg-base-100 shadow-xl">
+        <figure><img class="h-full w-60" src=${thumbnail_url} alt=""></figure>
+        <div class="card-body">
+            <h2 class="card-title">${title}</h2>
+            <p>${details.length > 200 ? details.slice(0, 200) + '...' : details}</p>
+            
+            <span class="flex items-center justify-between p-4">
+            <span class="flex items-center">
+            <img class="rounded-full h-1/12 w-1/12" src=${author.img} alt="">
+            <span class="mx-4">${author.name}</span>
+            </span>
+            <span class="mx-4"><i class="fa-solid fa-eye"></i> <span>${rating.number}</span></span>
+            <label for="my-modal-3" onClick="showModal('${title}')" class="btn modal-buttoncard-actions justify-end"><i class="fa-solid fa-arrow-right"></i></label>  
+            </span>     
+        </div>
+    </div>
+        `;
+        newsContainer.appendChild(newsDiv);
+    });
+}
+const loadNewsDetails = async (newsId) => {
+    const url = `https://openapi.programming-hero.com/api/news/${newsId}`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayNewsDetails(data.data);
+}
+const displayNewsDetails = news => {
+    console.log(news);
+}
+
+const showModal = (title, details) => {
+    const modalBody = document.getElementById('modal-body');
+    modalBody.textContent = '';
+    modalBody.innerHTML = `
+    <h3 class="text-lg font-bold">${title}</h3>
+    <p class="py-4">${details}</p>`
 }
