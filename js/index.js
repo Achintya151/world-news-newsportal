52b215e7db3d20b83bc0d137908f4604620ae4f5
Achintya151 <achintyakumartalukdar@gmail.com>
2022-09-03 16:53:49 +0600

    category section created

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
-console.log('connected');
+// console.log('connected');
+// Load category data 
+const loadAllCategories = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/news/categories');
+    const category = await res.json();
+    // console.log(category.data.news_category);
+    return (category.data.news_category);
+}
+// loadAllCategories();
+// set menu titles 
+const setAllMenu = async () => {
+    const category = await loadAllCategories();
+    // console.log(category);
+    const menu = document.getElementById('all-category');
+    const uniqueArray = [];
+    for (const data of category) {
+        console.log(data.category_name);
+        if (uniqueArray.indexOf(data.category_name) === -1) {
+            uniqueArray.push(data.category_name);
+            const li = document.createElement('li');
+            li.innerHTML = `<a>${data.category_name}</a>`
+            menu.appendChild(li);
+        }
+
+    }
+}
+setAllMenu();
